a3741b5eb1a55988afa279fdd260c6ffe64aa2d5
DESKTOP-VUM307Q\Romano <no.reply4me@gmail.com>
2019-01-21 19:37:24 +0100

    first implementation of the config class

---
+++ Client/VV/VV/ConfigWindow/Config.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VV.ConfigWindow
+{
+    class Config
+    {
+        public String User { get { return user; } set { user = value; } }
+        public String Password { get { return password; } set { EncryptPassword(value); } } // from user input
+        public String SetAllreadyEncryptedPassword { set { password = value; } } // Is needed to read the configs
+
+
+        private String user;
+        private String password;
+
+        /// <summary>
+        /// THis function hashes the userinput with the following method: 
+        /// </summary>
+        /// <param name="password">user input (password)</param>
+        private void EncryptPassword(String password)
+        {
+            String hashedPassword = "";
+            // TODO: Hash it / Salt it
+            password = hashedPassword;
+        }
+
+    }
+}
